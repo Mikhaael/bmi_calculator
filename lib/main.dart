6d9f56d1a8d54eb820538a1a007bc99b5554efbf
Mikhaeel <mikhaeelogunsanya77@gmail.com>
2022-07-12 08:32:22 -0700

    bmi_calculator:new app

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData(
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF0A0E21),
+        title: const Text('BMI CALCULATOR'),
+      ),
+      body: const Center(
+        child: Text('Body Text'),
+      ),
+    );
+  }
+}
